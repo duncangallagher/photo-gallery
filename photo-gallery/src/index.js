ca1d0bd7ca5624bd05d8d8afa7fe1b244f84c1c9
Duncan Gallagher <duncan@Duncans-MacBook-Pro.local>
2022-02-23 21:53:23 -0600

    working independent photo filters

--- photo-gallery/src/index.js
+++ photo-gallery/src/index.js
@@ -1,10 +1,171 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom';
 import Gallery from "react-photo-gallery";
-import { photos } from "./photos";
+import photos from "./photos";
 import './index.css';
 
+class Title extends React.Component {
+  render() {
+    return <h1>Travel Photos</h1>
+  }
+}
+
+class LocationFilter extends React.Component {
+
+  render() {
+
+    let options = [
+      {
+        label: "All",
+        value: "all",
+      },
+      {
+        label: "Japan",
+        value: "japan",
+      },
+      {
+        label: "Mexico",
+        value: "mexico",
+      },
+      {
+        label: "Europe",
+        value: "europe",
+      },
+      {
+        label: "Texas",
+        value: "texas",
+      },
+      {
+        label: "Oklahoma",
+        value: "oklahoma",
+      },
+      {
+        label: "California",
+        value: "california",
+      },
+      {
+        label: "Tennessee",
+        value: "tennessee",
+      },
+      {
+        label: "Colorado",
+        value: "colorado",
+      },
+      {
+        label: "Washington",
+        value: "washington",
+      },
+      {
+        label: "Florida",
+        value: "florida",
+      },
+    ];
+
+    return <div className="filterDiv">
+      <label htmlFor="locationFilter">Location: </label>
+      <select name="locations" id="locationFilter" className="selectFilter" onChange={this.props.onChange}>
+        {options.map((option) => (
+          <option value={option.value}>{option.label}</option>
+        ))}
+      </select>
+    </div>
+  }
+}
+
+class YearFilter extends React.Component {
+
+  render() {
+
+    let options = [
+      {
+        label: "All",
+        value: "all",
+      },
+      {
+        label: "2012",
+        value: "2012",
+      },
+      {
+        label: "2018",
+        value: "2018",
+      },
+      {
+        label: "2019",
+        value: "2019",
+      },
+      {
+        label: "2020",
+        value: "2020",
+      },
+      {
+        label: "2021",
+        value: "2021",
+      },
+    ];
+
+    return <div className="filterDiv">
+      <label htmlFor="yearFilter">Year: </label>
+      <select name="years" id="yearFilter" className="selectFilter" onChange={this.props.onChange}>
+        {options.map((option) => (
+          <option value={option.value}>{option.label}</option>
+        ))}
+      </select>
+    </div>
+  }
+}
+
+class App extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      photos_to_render: photos
+    }
+  }
+
+  filterByLocation (value) {
+    let filtered_location = value;
+    if (filtered_location != "all") {
+      let filtered_photos = photos.filter(photo => photo.location == filtered_location);
+      this.setState({
+        photos_to_render: filtered_photos
+      });
+    } else {
+      this.setState({
+        photos_to_render: photos
+      });
+    }
+  }
+
+  filterByYear (value) {
+    let filtered_year = value;
+    console.log(filtered_year);
+    if (filtered_year != "all") {
+      let filtered_photos = photos.filter(photo => photo.year == filtered_year);
+      console.log(filtered_photos)
+      this.setState({
+        photos_to_render: filtered_photos
+      });
+    } else {
+      this.setState({
+        photos_to_render: photos
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <Title />
+        <LocationFilter onChange={() => this.filterByLocation(document.getElementById('locationFilter').value)}/>
+        <YearFilter onChange={() => this.filterByYear(document.getElementById('yearFilter').value)}/>
+        <Gallery photos={this.state.photos_to_render} direction={"column"} />
+      </div>
+    )
+  }
+}
+
 ReactDOM.render(
-  <Gallery photos={photos} direction={"column"}/>,
+  <App />,
   document.getElementById('root')
 );
--- photo-gallery/src/photos.js
+++ photo-gallery/src/photos.js
@@ -1,248 +1,248 @@
-export const photos = [
+const photos = [
   {
     src: "/photos/japan_1.jpg",
     width: 3,
     height: 2,
     location: "japan",
-    "year": 2019
+    "year": "2019"
   },
   {
     src: "/photos/japan_2.jpg",
     width: 4,
     height: 5,
     location: "japan",
-    "year": 2019
+    "year": "2019"
   },
   {
     src: "/photos/japan_3.jpg",
     width: 3,
     height: 2,
     location: "japan",
-    "year": 2019
+    "year": "2019"
   },
   {
     src: "/photos/japan_4.jpg",
     width: 2,
     height: 3,
     location: "japan",
-    "year": 2019
+    "year": "2019"
   },
   {
     src: "/photos/japan_5.jpg",
     width: 5,
     height: 4,
     location: "japan",
-    "year": 2019
+    "year": "2019"
   },
   {
     src: "/photos/japan_6.jpg",
     width: 4,
     height: 3,
     location: "japan",
-    "year": 2019
+    "year": "2019"
   },
   {
     src: "/photos/dallas.jpg",
     width: 3,
     height: 5,
     location: "texas",
-    "year": 2018
+    "year": "2018"
   },
   {
     src: "/photos/bar.jpg",
     width: 4,
     height: 3,
     location: "texas",
-    "year": 2018
+    "year": "2018"
   },
   {
     src: "/photos/athens_1.jpg",
     width: 4,
     height: 3,
     location: "texas",
-    "year": 2020
+    "year": "2020"
   },
   {
     src: "/photos/athens_2.jpg",
     width: 4,
     height: 3,
     location: "texas",
-    "year": 2020
+    "year": "2020"
   },
   {
     src: "/photos/broken_bow.jpg",
     width: 2,
     height: 1,
     location: "oklahoma",
-    "year": 2018
+    "year": "2018"
   },
   {
     src: "/photos/sunflowers.jpg",
     width: 4,
     height: 3,
     location: "texas",
-    "year": 2019
+    "year": "2019"
   },
   {
     src: "/photos/fw.jpg",
     width: 4,
     height: 3,
     location: "texas",
-    "year": 2018
+    "year": "2018"
   },
   {
     src: "/photos/shell.jpg",
     width: 1,
     height: 1,
     location: "california",
-    "year": 2018
+    "year": "2018"
   },
   {
     src: "/photos/sunset.jpg",
     width: 3,
     height: 2,
     location: "oklahoma",
-    "year": 2018
+    "year": "2018"
   },
   {
     src: "/photos/flying.jpg",
     width: 4,
     height: 5,
     location: "california",
-    "year": 2018
+    "year": "2018"
   },
   {
     src: "/photos/sf_1.jpg",
     width: 4,
     height: 3,
     location: "california",
-    "year": 2018
+    "year": "2018"
   },
   {
     src: "/photos/sf_2.jpg",
     width: 4,
     height: 3,
     location: "california",
-    "year": 2018
+    "year": "2018"
   },
   {
     src: "/photos/sf_4.jpg",
     width: 4,
     height: 3,
     location: "california",
-    "year": 2018
+    "year": "2018"
   },
   {
     src: "/photos/sf_5.jpg",
     width: 3,
     height: 4,
     location: "california",
-    "year": 2018
+    "year": "2018"
   },
   {
     src: "/photos/sf_3.jpg",
     width: 4,
     height: 3,
     location: "california",
-    "year": 2018
+    "year": "2018"
   },
   {
     src: "/photos/sf_7.jpg",
     width: 4,
     height: 4,
     location: "california",
-    "year": 2018
+    "year": "2018"
   },
   {
     src: "/photos/sf_6.jpg",
     width: 4,
     height: 3,
     location: "california",
-    "year": 2018
+    "year": "2018"
   },
   {
     src: "/photos/cruise.jpg",
     width: 5,
     height: 4,
     location: "mexico",
-    "year": 2018
+    "year": "2018"
   },
   {
     src: "/photos/rollercoaster.jpg",
     width: 3,
     height: 2,
     location: "california",
-    "year": 2018
+    "year": "2018"
   },
   {
     src: "/photos/beach_1.jpg",
     width: 4,
     height: 3,
     location: "mexico",
-    "year": 2018
+    "year": "2018"
   },
   {
     src: "/photos/europe_1.jpg",
     width: 3,
     height: 2,
-    location: "germany",
-    "year": 2012
+    location: "europe",
+    "year": "2012"
   },
   {
     src: "/photos/europe_2.jpg",
     width: 3,
     height: 2,
-    location: "italy",
-    "year": 2012
+    location: "europe",
+    "year": "2012"
   },
   {
     src: "/photos/tn_1.jpg",
     width: 4,
     height: 3,
     location: "tennessee",
-    "year": 2019
+    "year": "2019"
   },
   {
     src: "/photos/tn_3.jpg",
     width: 3,
     height: 2,
     location: "tennessee",
-    "year": 2019
+    "year": "2019"
   },
   {
     src: "/photos/tn_2.jpg",
     width: 4,
     height: 3,
     location: "tennessee",
-    "year": 2019
+    "year": "2019"
   },
   {
     src: "/photos/tn_4.jpg",
     width: 3,
     height: 4,
     location: "tennessee",
-    "year": 2019
+    "year": "2019"
   },
   {
     src: "/photos/tn_5.jpg",
     width: 4,
     height: 3,
     location: "tennessee",
-    "year": 2019
+    "year": "2019"
   },
   {
     src: "/photos/tn_7.jpg",
     width: 5,
     height: 4,
     location: "tennessee",
-    "year": 2019
+    "year": "2019"
   },
   {
     src: "/photos/austin.jpg",
     width: 4,
     height: 3,
     location: "texas",
-    "year": 2018
+    "year": "2018"
 
   },
   {
@@ -250,70 +250,71 @@ export const photos = [
     width: 3,
     height: 4,
     location: "washington",
-    "year": 2021
+    "year": "2021"
   },
   {
     src: "/photos/seattle_3.jpg",
     width: 3,
     height: 5,
     location: "washington",
-    "year": 2021
+    "year": "2021"
   },
   {
     src: "/photos/seattle_5.jpg",
     width: 4,
     height: 3,
     location: "washington",
-    "year": 2021
+    "year": "2021"
   },
   {
     src: "/photos/seattle_4.jpg",
     width: 3,
     height: 4,
     location: "washington",
-    "year": 2021
+    "year": "2021"
   },
   {
     src: "/photos/seattle_1.jpg",
     width: 4,
     height: 3,
     location: "washington",
-    "year": 2021
+    "year": "2021"
   },
   {
     src: "/photos/flowers.jpg",
     width: 4,
     height: 3,
     location: "washington",
-    "year": 2021
+    "year": "2021"
   },
   {
     src: "/photos/beach_2.jpg",
     width: 4,
     height: 3,
     location: "texas",
-    "year": 2021
+    "year": "2021"
   },
   {
     src: "/photos/co_2.jpg",
     width: 4,
     height: 3,
     location: "colorado",
-    "year": 2021
+    "year": "2021"
   },
   {
     src: "/photos/co.jpg",
     width: 5,
     height: 3,
     location: "colorado",
-    "year": 2021
+    "year": "2021"
   },
   {
     src: "/photos/beach_3.jpg",
     width: 5,
     height: 3,
     location: "florida",
-    "year": 2019
+    "year": "2019"
   },
 ];
 
+export default photos;
